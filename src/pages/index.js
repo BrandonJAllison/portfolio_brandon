8d532462ecaed968617a6d10e86b0a3f27563188
Brandon Allison <brandona8128@outlook.com>
2019-05-11 21:23:27 -0400

    making some changes to the header

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,7 @@ import { Link } from "gatsby"
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
-import { IconContext } from "react-icons";
+
  
 
 
@@ -11,11 +11,11 @@ const IndexPage = () => (
   <Layout>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
     <h1>Page Under Construction, Please Check Back Soon!</h1>
-    <IconContext.Provider value={{ color: "blue", className: "global-class-name" }}>
+   
   <div>
-    <FaFolder />
+   
   </div>
-</IconContext.Provider>
+
     
     {/* <Link to="/page-2/">Go to page 2</Link> */}
   </Layout>
