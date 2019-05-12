bdc0e002521977b7e60f74c484139b4b19cc3303
Brandon Allison <brandona8128@outlook.com>
2019-05-11 21:05:09 -0400

    changes made

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,21 @@
 import React from "react"
 import { Link } from "gatsby"
-
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
+import { IconContext } from "react-icons";
+ 
+
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
     <h1>Page Under Construction, Please Check Back Soon!</h1>
-    <p><i class="fas fa-tools"></i></p>
+    <IconContext.Provider value={{ color: "blue", className: "global-class-name" }}>
+  <div>
+    <FaFolder />
+  </div>
+</IconContext.Provider>
     
     {/* <Link to="/page-2/">Go to page 2</Link> */}
   </Layout>
