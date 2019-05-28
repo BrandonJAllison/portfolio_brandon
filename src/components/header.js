e99e6dcc904b618479f99e33f587ee9f3f7dad33
Brandon Allison <brandona8128@outlook.com>
2019-05-27 21:57:11 -0400

    changes to the header

--- src/components/header.js
+++ src/components/header.js
@@ -3,20 +3,29 @@ import PropTypes from "prop-types"
 import React from "react"
 
 
+
+
 const Header = ({ siteTitle }) => (
   
   <header
     style={{
       background: `black`,
+      padding: "15px 0",
+      boxShadow: "0 8px 6px -6px #999"
     }}
   >
   
     <div
       style={{
         maxWidth: 960,
+        
       }}
     >
-      <p style={{color: "white", padding: ".5rem"}}>About | Projects | Contact</p>
+      <Link style={{color: "white", padding: ".5rem", textDecoration: "none"}}>About</Link>
+      <Link style={{color: "white", padding: ".5rem", textDecoration: "none"}}>Projects</Link>
+      <Link style={{color: "white", padding: ".5rem", textDecoration: "none"}}>Contact</Link>
+      
+    
       
     </div>
     
