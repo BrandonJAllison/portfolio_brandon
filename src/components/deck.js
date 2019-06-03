6412060211a317642224829a088238a236a825af
Brandon Allison <brandona8128@outlook.com>
2019-06-03 12:22:20 -0400

    change

--- src/components/deck.js
+++ src/components/deck.js
@@ -10,7 +10,6 @@ const cards = [
   'https://cdn.pixabay.com/photo/2018/05/08/21/08/html5-3383934_960_720.png'
 ]
 
-// These two are just helpers, they curate spring data, values that are later being interpolated into css
 const to = i => ({ x: 0, y: i * -4, scale: 1, rot: -10 + Math.random() * 20, delay: i * 100 })
 const from = i => ({ x: 0, rot: 0, scale: 1.5, y: -1000 })
 // This is being used down there in the view, it interpolates rotation and scale into a css transform
